83a77a8d2f1edb4014967ea51926ec4d0dbf4d02
Iva Panic <iva.panic@fer.hr>
2020-04-26 00:07:41 +0200

    Finished 2nd labassignment (Perl), added pdf

--- LAB2/zadatak5.pl
+++ LAB2/zadatak5.pl
@@ -15,6 +15,36 @@ if (! open FILE, $file) {
 	die "The file cannot be found\n";
 }
 
+sub check_coeffs {
+if ($i == 1) {
+		@coeffs = split /;/, $_;
+	}
+}
+
+sub calculate {
+	if ($i >= 4) {
+		@elements = split /;/, $_;
+		$jmbag = shift @elements;
+		$surname = shift @elements;
+		$name = shift @elements;
+		
+		$grades_sum = 0;
+		$index = 0;
+		do {
+			$is_none = $elements[$index] eq '-';
+			if (! $is_none) {
+				$grades_sum += $elements[$index] * $coeffs[$index];
+			}
+			$index++;
+		} while ($index <= 6);
+
+		$key = " $surname, $name ($jmbag)";
+        	$rang{$key} = $grades_sum;
+
+	}
+}
+
+
 sub sorted_output {
 	$range = 1;
 	print "Lista po rangu:\n";
@@ -31,30 +61,8 @@ $i = 0;
 
 while(<FILE>){
 	
-	if ($i == 1) {
-		@factors = split /;/, $_;
-	}
-
-	if ($i >= 4) {
-		@elements = split /;/, $_;
-		$jmbag = shift @elements;
-		$surname = shift @elements;
-		$name = shift @elements;
-		
-		$grades_sum = 0;
-		$index = 0;
-		do {
-			$is_none = $elements[$index] eq '-';
-			if (! $is_none) {
-				$grades_sum += $elements[$index] * $factors[$index];
-			}
-			$index++;
-		} while ($index <= 6);
-
-		$key = " $surname, $name ($jmbag)";
-        	$rang{$key} = $grades_sum;
-
-	}
+	check_coeffs;
+	calculate;
 
 	$i++;
 		
